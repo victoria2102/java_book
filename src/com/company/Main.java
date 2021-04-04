cae3c44c94aa11533f73c62f7ab654775763add1
Victoria <trebis.victoria@gmail.com>
2021-04-04 13:25:41 +0300

    add true system out println position

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,28 +7,22 @@ public class Main {
         int i = 1;
         int s = 0;
 
-        while (i<=100 ) {
+        while (i <= 100) {
             i++;
             s = s + i;
-            System.out.println(s);
-
-
         }
+        System.out.println("while : " + s);
 
         do {
             i++;
-            s=s+i;
-        } while (i<=100);
-        System.out.println( s );
+            s = s + i;
+        } while (i <= 100);
+        System.out.println("do : " + s);
 
         for (i = 1; i <= 100; i++) {
             s = s + i;
-            System.out.println(s);
-
-
         }
+        System.out.println("for : " + s);
     }
-
-
-    }
+}
 
