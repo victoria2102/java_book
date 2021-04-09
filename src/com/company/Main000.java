74e265eaed3ef15f35443bd5fe87ad7b9c2c8434
Victoria <trebis.victoria@gmail.com>
2021-04-09 19:02:54 +0300

    add scanner

--- src/com/company/Main000.java
+++ src/com/company/Main000.java
@@ -1,33 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main000 {
 
     public static void main(String[] args) {
         // write your code here
-        int i;
-        int s;
+        int s=0;
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        int i = console.nextInt();
+        int c = console.nextInt();
+        int b = console.nextInt();
 
-        i = 0;
-        s = 0;
-        while (i <= 100) {
-            s = s + i;
-            i++;
-        }
-        System.out.println("while : " + s);
 
-        i = 0;
-        s = 0;
         do {
             i++;
             s = s + i;
         } while (i < 100);
+        s = s + 1;
         System.out.println("do : " + s);
 
+
         s = 0;
-        for (i = 0; i <= 100; i++) {
+        while (c <= 100) {
+            s = s + c;
+            c++;
+        }
+        System.out.println("while : " + s);
+
+
+
+        s = 0;
+        for (i = b; i <= 100; i++) {
             s = s + i;
         }
         System.out.println("for : " + s);
     }
+
 }
+
 
