169b16e4ce19f275a07248981eab2dc0ba1fe76a
Victoria <trebis.victoria@gmail.com>
2021-04-10 21:14:14 +0300

    add n

--- src/com/company/Main000.java
+++ src/com/company/Main000.java
@@ -6,32 +6,32 @@ public class Main000 {
 
     public static void main(String[] args) {
         // write your code here
-        int s = 0;
+        int s;
+        int i = 0;
         Scanner console = new Scanner(System.in);
         System.out.println("Введите целое число:");
-        int i = console.nextInt();
-        int c = console.nextInt();
-        int b = console.nextInt();
+        int n = console.nextInt();
 
 
+       s = 0;
         do {
             i++;
             s = s + i;
-        } while (i < 100);
+        } while (i < n);
         s = s + 1;
         System.out.println("do : " + s);
 
-
+        i = 0;
         s = 0;
-        while (c <= 100) {
-            s = s + c;
-            c++;
+        while (i <= n) {
+            s = s + i;
+            i++;
         }
         System.out.println("while : " + s);
 
 
         s = 0;
-        for (i = b; i <= 100; i++) {
+        for (i = 0; i <= n; i++) {
             s = s + i;
         }
         System.out.println("for : " + s);
